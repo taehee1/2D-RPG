6e714391aa0af0256bcd7425b1979e207eaf1b60
leechuanhung <163071930+taehee1@users.noreply.github.com>
2024-05-22 16:05:22 +0900

    메인 씬

--- Assets/Script/SelectCharacter.cs
+++ Assets/Script/SelectCharacter.cs
@@ -1,17 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class SelectCharacter : MonoBehaviour
 {
-    public Text name;
-    public Text feature;
+    public Text nameTxt;
+    public Text featureTxt;
     public Image charImage;
 
     public GameObject[] characters;
+    public CharacterInfo[] characterInfos;
     private int charIndex = 0;
+
+    public GameObject gameStart;
+    public Text gameCountTxt;
+    private bool isPlayClick = false;
+    private float gameCount = 5f;
+
+    public static string characterName;
+
+    private void Update()
+    {
+        if (isPlayClick)
+        {
+            gameCount -= Time.deltaTime;
+            if (gameCount < 0)
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+            gameCountTxt.text = $"곧 게임이 시작됩니다. \n {gameCount:F1}";
+        }
+    }
+
+    public void PlayBtn()
+    {
+        gameStart.SetActive(true);
+        isPlayClick = true;
+        characterName = characters[charIndex].name;
+    }
+
+    private void Start()
+    {
+        SetInfo();
+    }
 
     public void SelectCharacterBtn(string btnName)
     {
@@ -31,5 +65,13 @@ public class SelectCharacter : MonoBehaviour
 
         Debug.Log($"{charIndex}");
         characters[charIndex].SetActive(true);
+        SetInfo();
+    }
+
+    private void SetInfo()
+    {
+        nameTxt.text = characterInfos[charIndex].Name;
+        featureTxt.text = characterInfos[charIndex].Feature;
+        charImage.sprite = characters[charIndex].GetComponent<SpriteRenderer>().sprite;
     }
 }
